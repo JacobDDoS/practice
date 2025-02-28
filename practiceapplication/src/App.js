777318fe8f03122f7a0ba3829431266e743d648e
Thomas Schaefer <notthomasschaefer@gmail.com>
2025-02-28 14:24:22 -0600

    thomas half

--- practiceapplication/src/App.js
+++ practiceapplication/src/App.js
@@ -1,10 +1,12 @@
 import './CSS/App.css';
 import EthanHalf from './Components/EthanHalf';
+import ThomasHalf from './Components/ThomasHalf';
 
 function App() {
   return (
     <div className="App">
       <EthanHalf />
+      <ThomasHalf />
     </div>
   );
 }
